a4e20c8385b354c4d28bedde56b343a38ed8493d
dexter ramos <blitzdex27@gmail.com>
2025-09-07 12:11:40 +0800

    Initial Commit

---
+++ Spirit Pouch/Spirit_PouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spirit_PouchApp.swift
+//  Spirit Pouch
+//
+//  Created by Dexter Ramos on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Spirit_PouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
